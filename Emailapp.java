18b6041f3958d61d6b077d957f9e4e6a81a662cd
Arunika Srivastava <aru26srivastava001@gmail.com>
2023-03-12 23:27:47 +0530

    Update Emailapp.java

--- Emailapp.java
+++ Emailapp.java
@@ -1,6 +1,5 @@
 package emailapp;
 public class Emailapp{
-
   public static void main(String[] args) {
         Email obj = new Email("Devang","Yadav"); 
         System.out.println("Your changed password is" + obj.getchangePassword());
